4b9f65472eecfb56993b705065b05576e0ab6427
kkinge79 <kingerykagan@gmail.com>
2021-11-18 21:28:27 -0600

    home.hmtl

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-# Create your views here.
+
+
+class Finch:  
+  def __init__(self, name, sex, description, color):
+    self.name = name
+    self.sex = sex
+    self.description = description
+    self.color = color
+
+finches = [
+  Finch('Dee', 'female', 'Kinda rude.', 'brown'),
+  Finch('Big Bird', 'male', 'Looks like a robot.', 'yellow'),
+  Finch('gogurt', 'female', 'Happy fluff ball.', 'blue'),
+  Finch('Bico', 'female', 'chirps loudly.', 'red')
+]
 def home(request):
   return HttpResponse('<h1>HELLO WORLD</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the finchCollector🐦</h1>')
-def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {'finches' : finches })
