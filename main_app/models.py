4129c842fed129fe98cfde1f74e099ef26de76ab
kkinge79 <kingerykagan@gmail.com>
2021-11-18 21:43:49 -0600

    add all finch complete, need images

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Finch(models.Model):
+  name = models.CharField(max_length=100)
+  sex = models.CharField(max_length=20)
+  description = models.TextField(max_length=250)
+  color = models.TextField(max_length=30)
--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from .models import Finch
 
 
-class Finch:  
-  def __init__(self, name, sex, description, color):
-    self.name = name
-    self.sex = sex
-    self.description = description
-    self.color = color
-
-finches = [
-  Finch('Dee', 'female', 'Kinda rude.', 'brown'),
-  Finch('Big Bird', 'male', 'Looks like a robot.', 'yellow'),
-  Finch('gogurt', 'female', 'Happy fluff ball.', 'blue'),
-  Finch('Bico', 'female', 'chirps loudly.', 'red')
-]
 def home(request):
-  return HttpResponse('<h1>HELLO WORLD</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {'finches' : finches })
+
+def home(request):
+  return render(request, 'home.html')
